4ac104168e95dab754330f410a0566d932de6f22
Pierre Fontecha <Pierre.Fontecha@gmail.com>
2021-01-09 14:16:12 -0800

    added cmd line input for user

---
+++ index.js
@@ -0,0 +1,67 @@
+//Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const licChoices = ["MIT", "GNU"];
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What is the description?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What are your installation instructions?',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Usage?',
+            name: 'usage'
+        },
+        
+        {
+            type: 'list',
+            name:"license",
+            message: 'Which license?',
+            choice: licChoices,
+        },
+        {
+            type: 'input',
+            message: 'What about contributors?',
+            name: 'contribution'
+        },
+                
+    ])
+    .then(answers => {
+        console.log(answers)
+        const markdownOutput = `generateMarkdown.js`
+        console.log(markdownOutput);
+        fs.writeFile(`${answers.name}.md`, markdownOutput, function(err) {
+            if(err) {
+                console.log(err)
+            }else {
+                console.log("markdown done!")
+            }
+            
+    });
+
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
