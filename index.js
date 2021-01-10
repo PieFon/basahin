02cfadfbbd862ef021523b48defe711599b7758e
Pierre Fontecha <Pierre.Fontecha@gmail.com>
2021-01-09 17:37:10 -0800

    edited markdown

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const markdownOutput = require("./utils/generateMarkdown.js");
 const licChoices = ["MIT", "GNU"];
+const markdownTemplate = `
 
-// TODO: Create an array of questions for user input
-const questions = [];
+`
 
 inquirer
     .prompt([
@@ -29,33 +29,68 @@ inquirer
             message: 'Usage?',
             name: 'usage'
         },
-        
+
         {
             type: 'list',
-            name:"license",
+            name: "license",
             message: 'Which license?',
-            choice: licChoices,
+            choices: licChoices,
         },
         {
             type: 'input',
             message: 'What about contributors?',
             name: 'contribution'
         },
-                
-    ])
-    .then(answers) => {
-        console.log(answers)
-        const markdownOutput = `generateMarkdown.js`
-        console.log(markdownOutput);
-        fs.writeFile(`${answers.name}.md`, markdownOutput, function(err) {
-            if(err) {
-                console.log(err)
-            }else {
-                console.log("markdown done!")
-            }
-            
-    };
 
+    ])
+    .then(answers => {
+        console.log(answers);
+        
+        const markdownOutput = `
+
+  ### ${answers.title}\n
+
+  ## Description ${answers.description}\n
+
+  ## Table Of Contents 
+  * [Installation](#Installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation\n
+  $(answers.installation)\n
+
+  ## Usage \n
+  $(answers.usage)\n
+
+  ## License \n
+  $(answers.license)\n
+
+  ## Contributors \n
+  $(answers.contributors) \n
+
+  ## Tests \n
+  $(answers.tests) \n
+
+  ## Questions \n
+  $(answers.questions) \n
+
+  `;
+
+         
+        ;
+        fs.writeFile(`${answers.name}.md`, markdownOutput, function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Markdown file created, you are done!")
+            }
+
+        });
+    });
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
